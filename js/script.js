1c0689bea7388f34175314d401753f810b8b74f5
Kyle <greekdanceparty@gmail.com>
2023-01-23 15:54:33 -0700

    added issues list with tooltip

--- js/script.js
+++ js/script.js
@@ -1,23 +1,18 @@
 //load data
-allData = d3.json('./data/data.json').then((data) => {
-    //load elements...
+allData = d3.json("./data/data.json").then((data) => {
+  //load elements...
 
-    // function from storytelling.js
-    textBox();
-    
-    //adding divs for elements...
-    let wrapperDiv = d3.select("body").append("div").attr("id", "wrapper");
+  //adding divs for elements...
+  let wrapperDiv = d3.select("body").append("div").attr("id", "wrapper");
 
-    //wrapperDiv.append("div").attr("id", "tooltip").style("opacity", 0); //idk why this was in the wrapper in the hw.
+  // function from storytelling.js
+  textBox();
 
-    let t2tDiv = wrapperDiv.append("div").attr("id", "t2t");
-    let gooeyDiv = wrapperDiv.append("div").attr("id", "gooey");
-    let errorDiv = wrapperDiv.append("div").attr("id", "error");
-    let commentDiv = wrapperDiv.append("div").attr("id", "comment");
+  let t2tDiv = wrapperDiv.append("div").attr("id", "t2t");
+  let gooeyDiv = wrapperDiv.append("div").attr("id", "gooey");
+  let errorDiv = wrapperDiv.append("div").attr("id", "error");
+  let commentDiv = wrapperDiv.append("div").attr("id", "comment");
 
-    //add vis elements to divs
-    let t2tChart = new TimeToTemp(t2tDiv, data);
-    console.log(data);
-
+  //add vis elements to divs
+  let t2tChart = new TimeToTemp(t2tDiv, data);
 });
-
--- js/storytelling.js
+++ js/storytelling.js
@@ -1,26 +1,41 @@
-function textBox () {
-    const headBlurb = `This webpage details the Whitebox testing`;
-    const textBlurb = ` 
+function textBox() {
+  const headBlurb = `This webpage details the Whitebox testing`;
+  const textBlurb = ` 
         performed on 45 Beta Model 2 Yosemite grills.  This goal of this testing was to determine the number of instances of 7 issues identified by customer service based on Model 4 service calls and the ability to fix these issues via an OTA firmware update. 
       `;
 
-     
-    const textDiv = d3.select('#header-wrap')
-      .append('div');
-    textDiv.style('opacity', 0)
-      .classed('text-blurb', true)
-      .transition()
-      .delay(700).style('opacity', 1);
+  const issueBlurb = "The issues targeted in this testing are the following:";
+
+  const textDiv = d3.select("#header-wrap").append("div");
+  textDiv
+    .style("opacity", 0)
+    .classed("text-blurb", true)
+    .transition()
+    .delay(700)
+    .style("opacity", 1);
+
+  textDiv.append("span").append("text").attr("opacity", 0).text(headBlurb);
+  textDiv.append("text").text(textBlurb);
   
-    textDiv.append('span').append('text')
-      .attr('opacity', 0)
-      .text(headBlurb);
-    textDiv.append('text').text(textBlurb);
-  }
-  function issuesBox (div) {
-  //What are the 7 issues?
-    
-  //which issues show up? which dont?
+  //need another spot for the issues...
+  const issueDiv = d3.select("#wrapper").append("div");
+  issueDiv.style("opacity", 0)
+  .classed("text-blurb", true)
+  .transition()
+  .delay(700)
+  .style("opacity", 1);
 
+  //issueDiv.append("html").html("</br>");
+  issueDiv.append("span").append("text").attr("opacity", 0).text(issueBlurb);
 
-  }
+  issueDiv.append("html").html(`<ol id="issues_list">
+  <li title = "This rarely occured, mainly if Gooey had out of date FW"> Display does not power on</li>
+  <li title = "This was common, especially with the 2206.M08 config on smalls">Slow to temp</li>
+  <li title = "Similar to number 1, this mainly happened if Gooey FW was out of date" >Frozen screen</li>
+  <li title = "This was the MOST COMMON issue to occur on the Factory FW but never occured on the OTA1 FW">Igniter not detected</li>
+  <li title = "This issue never occured">Frozen screen at "ready to cook"</li>
+  <li title = "This issue never occured">Grill seasoning skipped</li>
+  <li title = "This occured on one grill with a faulty sensor">Lid sesnsor error</li>
+  <li title = "This issue never occured">Flame sensor error</li>
+</ol>`)
+}
