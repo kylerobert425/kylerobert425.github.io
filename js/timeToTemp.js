747319f418065ecc996db8547ad969d3a54e5ca8
Kyle <greekdanceparty@gmail.com>
2023-01-25 11:12:33 -0700

    added color scale changed tooltip

--- js/timeToTemp.js
+++ js/timeToTemp.js
@@ -3,6 +3,9 @@ const margin = { top: 10, right: 30, bottom: 30, left: 60 },
   width = 460 - margin.left - margin.right,
   height = 400 - margin.top - margin.bottom;
 
+//color scale
+const color = d3.scaleOrdinal().domain(['2206.001', '2205.001']).range(["#1f78b4", "#7dad6a"]);
+
 //constructor
 class TimeToTemp {
   constructor(div, data) {
@@ -20,6 +23,8 @@ class TimeToTemp {
     this.x = d3.scaleLinear().domain(domain).range([0, width]);
 
     this.y = d3.scaleLinear().domain(domain).range([height, 0]);
+
+
 
     this.setup();
 
@@ -55,7 +60,7 @@ class TimeToTemp {
       .attr("cx", (d) => this.x(d.t2t_1))
       .attr("cy", (d) => this.y(d.t2t_2))
       .attr("r", 6)
-      .style("fill", "black")
+      .style("fill", (d) => color(d.config))
       .style("opacity", 0.7)
       .on("mouseover", addToolTip)
       .on("mouseleave", removeToolTip);
@@ -68,20 +73,26 @@ function removeToolTip () {
         .duration(500)
         .style("opacity", 0);
     //remove highlight dot
-    d3.select(this).style("stroke", "black").style("stroke-width", 0.8)
+    d3.select(this).style("stroke", (d) => color(d.config)).style("stroke-width", 0.8)
 }
 
 function addToolTip (d) {
+  let size = '';
+  size = d.config === "2206.001" ?  "Small" : "Large";
+
     d3.select("#tooltip")
         .transition()
         .duration(200)
         .style("opacity", 0.9);
     d3.select("#tooltip")
-        .html("UUID is: " + d.uuid + "<br/>" +
-            "Factory Time to temp: " + Math.round(d.t2t_1/60) +"<br/>" +
-            "OTA Time to Temp: " + Math.round(d.t2t_2/60))
+        //this is getting a bit verbose....
+        .html("Grill size - " + size + "<br/>" +
+            "Factory Time to temp: " + Math.round(d.t2t_1/60) + "<br/>" +
+            "Factory Config: " + d.config + "<br/>" +
+            "OTA Time to Temp: " + Math.round(d.t2t_2/60) + "<br/>" +
+            "OTA Config: " + d.config_2)
         .style("left", `${d3.event.pageX + 15}px`)
         .style("top", `${d3.event.pageY - 10}px`);
     //add highlight dot
-    d3.select(this).style("stroke", "black").style("stroke-width", 3)
+    d3.select(this).style("stroke", (d) => color(d.config)).style("stroke-width", 4)
 }
